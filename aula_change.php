d7ba192bc9ecee871de5a6dcbde53e8b9f13bd3f
zlost <suporte2@o2multi.com.br>
2015-12-20 21:14:38 -0200

    fim modulo 2

--- aula_change.php
+++ aula_change.php
@@ -5,7 +5,7 @@
         <script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.0.8/angular.min.js"></script>
     </head>
     <body>
-
+        <!-- Change -->
         <div ng-controller="CtrlApp">
             <input type="text" ng-model="nome" ng-change="loggar()"/>
             <h1>{{nome}} - {{idade}}, {{cidade}}</h1>
@@ -20,5 +20,35 @@
                 };
             };
         </script>
+
+        <!-- Change -->
+
+
+        Busca: <input type="search" ng-model="busca">
+        <div ng-controller="CtrlLista">
+            <select ng-model="selected" ng-options="pessoa.nome for pessoa in pessoas | filter: busca">
+                <option value="">Selecione</option>
+            </select>
+
+            <hr>
+            <div>
+                Nome: <input type="text" ng-model="selected.nome">
+                Cidade: <input type="text" ng-model="selected.cidade">
+            </div>
+
+
+        </div>
+        <script>
+            var CtrlLista = function ($scope) {
+                $scope.pessoas = [
+                    {nome: "Lucas", cidade: "São Paulo"},
+                    {nome: "Maria", cidade: "SP"},
+                    {nome: "João", cidade: "FP"},
+                    {nome: "Gabriel", cidade: "Minas"}
+                ];
+            };
+
+        </script>
+
     </body>
 </html>
